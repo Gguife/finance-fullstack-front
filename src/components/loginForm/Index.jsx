a6d19cc19bc3a893db5a52c70c2e3b44105ddf02
Gguife <gguife747@gmail.com>
2024-02-06 16:44:42 -0300

    feat: Create Login form component

--- src/components/loginForm/Index.jsx
+++ src/components/loginForm/Index.jsx
@@ -1,24 +1,42 @@
 'use client';
+import { useState } from 'react';
+import axios from "axios";
+import * as S from "./Style";
 
 const LoginForm = () =>{
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-  const onSubmit = (e) =>{
+  const onChangeValue = (e) =>{
+    const {name, value} = e.target;
+
+    if( name === "email" ) setEmail(value);
+    if( name === "password" ) setPassword(value);
+  
+  }
+
+  const onSubmit = async (e) =>{
     e.preventDefault();
-    console.log("Formulario Enviado!");
+    
+    try{
+      const response = await axios.post("http://localhost:8080/auth/login", {email, password})
+      localStorage.setItem("token", response.data.data.token);
+    }
+    catch(error){
+      console.log("error", error);
+    }
+    
   }
 
   return(
-    <form onSubmit={onSubmit}>
-      <h1>Login Page</h1>
-
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" />
-
-      <label htmlFor="password">Senha</label>
-      <input type="password" id="password" name="password" />
-
-      <button type="submit">Entrar</button>
-    </form>
+    <S.FormLogin>
+      <form onSubmit={onSubmit}>
+        <h1>Login</h1>
+        <S.TextField type='email' id="email" label="email" name='email' variant="outlined" onChange={onChangeValue} />
+        <S.TextField type='password' id="password" label="password" name='password' variant="outlined" onChange={onChangeValue} />
+        <S.Button variant="contained" type="submit">Entrar</S.Button>
+      </form>
+    </S.FormLogin>
   )
 }
 
---
+++ src/components/loginForm/Style.jsx
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+import ButtonUI from "@mui/material/Button";
+import TextFieldUI from "@mui/material/TextField";
+
+export const FormLogin = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  margin: 0 auto;
+`;
+
+export const TextField = styled(TextFieldUI)`
+  border-radius: 10px;
+  width: 100%;
+`;
+export const Button = styled(ButtonUI)``;
