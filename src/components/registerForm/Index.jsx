d695268ecf945961fe596209bd64be6a8f8ff33a
Gguife <gguife747@gmail.com>
2024-02-06 16:44:54 -0300

    feat: Create Register form component

--- src/components/registerForm/Index.jsx
+++ src/components/registerForm/Index.jsx
@@ -1,27 +1,49 @@
 'use client';
+import { useState } from 'react';
+import axios from "axios";
+import * as S from "./Style";
 
 const RegisterForm = () =>{
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-  const onSubmit = (e) =>{
-    e.preventDefault();
-    console.log("Formulario Enviado!");
+  const onChangeValue = (e) =>{
+    const {name, value} = e.target;
+
+    if( name === "name" ) setName(value);
+    if( name === "email" ) setEmail(value);
+    if( name === "password" ) setPassword(value);
+  
   }
 
+  const onSubmit = async (e) =>{
+    e.preventDefault();
+    
+    try{
+      const response = await axios.post("http://localhost:8080/auth/register", {name, email, password})
+      localStorage.setItem("token", response.data.data.token);
+      console.log("response", response);
+    }
+    catch(error){
+      console.log("error", error);
+    }
+    
+  }
+
+
   return(
-    <form onSubmit={onSubmit}>
-      <h1>Register Page</h1>
+    <S.FormRegister>
+      <form onSubmit={onSubmit}>
+        <h1>Register Page</h1>
 
-      <label htmlFor="name">Nome</label>
-      <input type="name" id="name" name="name" />
+        <S.TextField type='name' id="name" label="name" name='name' variant="outlined" onChange={onChangeValue} />
+        <S.TextField type='email' id="email" label="email" name='email' variant="outlined" onChange={onChangeValue} />
+        <S.TextField type='password' id="password" label="password" name='password' variant="outlined" onChange={onChangeValue} />
 
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" />
-
-      <label htmlFor="password">Senha</label>
-      <input type="password" id="password" name="password" />
-
-      <button type="submit">Entrar</button>
-    </form>
+        <S.Button variant="contained" type="submit">Cadastrar</S.Button>
+      </form>
+    </S.FormRegister>
   )
 }
 
