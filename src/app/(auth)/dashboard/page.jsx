9738e445a1ca17862bb16bcd4132f38cef076be9
Gguife <gguife747@gmail.com>
2024-02-19 17:56:04 -0300

    feat: Created dashboard

--- src/app/(auth)/dashboard/page.jsx
+++ src/app/(auth)/dashboard/page.jsx
@@ -1,39 +1,35 @@
 'use client';
 
-import { useEffect } from "react";
-import axios from "axios";
-// import CategoriesCreate from "@/components/categories/categoriesCreate";
-import CategoriesUpdate from "@/components/categories/cateogiresUpdate";
+import axios from 'axios';
+import { useEffect, useState } from "react";
 
-const DashboardPage = () =>{
+
+export const DashboardPage = () => {
+  const [ user, setUser ] = useState({
+    id: null
+  });
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
-    if(!token){
-      window.location.href = "/login";
+    const token = localStorage.getItem('token');
+    if (!token) {
+      window.location.href = '/login';
     }
-
-    axios.get("http://localhost:8080/users/me", {
+    axios.get('http://localhost:8080/users/me', {
       headers: {
-        "Authorization": `Bearer ${token}`
+        'Authorization': `Bearer ${ token }`
       }
     }).then(response => {
-      console.log(response.data.data);
-    }).catch(error =>{
-      window.location.href = "/login";
+      setUser(response.data.data);
+    }).catch(_ => {
+      window.location.href = '/login';
     })
 
-  },[])
-
-
-  return(
+  }, [])
+  return (
     <div>
-      <h1>Dashboard</h1>
-      <CategoriesUpdate categoryId={3} />
+      <h1>Olá, { user.name }</h1>
     </div>
-  )
-
+  );
 }
-
 
 export default DashboardPage;
