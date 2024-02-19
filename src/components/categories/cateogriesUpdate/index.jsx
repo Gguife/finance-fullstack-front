a7f38770f36a64fae85dc96c91a8f85b6df07159
Gguife <gguife747@gmail.com>
2024-02-19 17:57:28 -0300

    feat: Create CategoriesUpdate and your style

---
+++ src/components/categories/cateogriesUpdate/index.jsx
@@ -0,0 +1,103 @@
+'use client'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import * as S from './style'
+
+export const CategoriesUpdate = ({ categoriaId }) => {
+  const [ nome, setNome ] = useState();
+  const [ userId, setUserId ] = useState();
+
+  const [ notification, setNotification ] = useState({
+    open: false,
+    message: '',
+    severity: ''
+  });
+
+  const onChangeValue = (e) => {
+    const { name, value } = e.target
+    if (name === 'nome') setNome(value)
+  }
+
+  useEffect(() => {
+    const getCategoria = async () => {
+      try {
+        const token = localStorage.getItem('token')
+        const response = await axios.get(`http://localhost:8080/categories/${ categoriaId }`, {
+          headers: {
+            Authorization: `Bearer ${ token }`
+          }
+        })
+        setNome(response.data.data.nome)
+        setUserId(response.data.data.user_id)
+      }
+      catch (error) {
+        let errorMessage = "Ocorreu um erro desconhecido.";
+        if (error.response && error.response.error) {
+          errorMessage = error.response.error;
+        }
+        setNotification({
+          open: true,
+          message: errorMessage,
+          severity: 'error'
+        });
+      }
+    }
+
+    getCategoria()
+  }, [ categoriaId ])
+
+  const onSumbmit = async (e) => {
+    e.preventDefault()
+    try {
+      const token = localStorage.getItem('token')
+      await axios.put(`http://localhost:8080/categories/${ categoriaId }`, { nome, user_id: userId }, {
+        headers: {
+          Authorization: `Bearer ${ token }`
+        }
+      })
+      setNotification({
+        open: true,
+        message: `Categoria ${ nome } atualizada com sucesso!`,
+        severity: 'success'
+      
+      })
+    }
+    catch (error) {
+      setNotification({
+        open: true,
+        message: error.response.data.error,
+        severity: 'error'
+      })
+    }
+  }
+
+  const handleClose = (_, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setNotification({
+      open: false,
+      message: '',
+      severity: ''
+    })
+  }
+
+  return (
+    <>
+      <S.Form onSubmit={ onSumbmit }>
+        <S.H1>Atualizar Categoria</S.H1>
+        <S.TextField name="nome" onChange={ onChangeValue } label="Nome" variant="outlined" value={ nome } color='primary' fullWidth />
+        <S.Button variant="contained" color="success" type="submit">Enviar</S.Button>
+      </S.Form>
+
+      <S.Snackbar open={ notification.open } autoHideDuration={ 3000 } onClose={ handleClose }>
+        <S.Alert onClose={ handleClose } severity={ notification.severity } variant="filled" sx={{ width: '100%' }}>
+          { notification.message }
+        </S.Alert>
+      </S.Snackbar>
+    </>
+  )
+}
+
+export default CategoriesUpdate
