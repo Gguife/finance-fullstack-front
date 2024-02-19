9ab3bfde0060796dc7c8d552b0fba839e95b61f8
Gguife <gguife747@gmail.com>
2024-02-19 17:56:46 -0300

    feat: Finished component categoriesCreate

--- src/components/categories/categoriesCreate/index.jsx
+++ src/components/categories/categoriesCreate/index.jsx
@@ -1,45 +1,99 @@
-'use client';
-import { useState } from "react";
-import * as S from "./style.jsx";
-import axios from "axios";
+'use client'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import * as S from './style'
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
 
 
-const CategoriesCreate = () =>{
-  const [name, setName] = useState();
-  const onChangeValue = (e) =>{
-    const {name, value} = e.target;
+export const CategoriesCreate = ({ openModal, closeModal }) => {
+  const [ nome, setNome ] = useState();
 
-    if( name === "name" ) setName(value);
+  const [ notification, setNotification ] = useState({
+    open: false,
+    message: '',
+    severity: ''
+  });
 
-  
+  const [ open, setOpen ] = useState(false);
+
+  useEffect(() => {
+    if(openModal) {
+      setOpen(true);
+    }
+  }, [ openModal ])
+
+  const handleCloseModal = () => {
+    setOpen(false);
+    closeModal(false);
+  };
+
+  const onChangeValue = (e) => {
+    const { name, value } = e.target
+    if (name === 'nome') setNome(value)
   }
 
-  const onSubmit = async (e) =>{
-    e.preventDefault();
-    
-    try{
-      const token = localStorage.getItem("token");
-      await axios.post("http://localhost:8080/categories", { name }, {
+  const onSumbmit = async (e) => {
+    e.preventDefault()
+    try {
+      const token = localStorage.getItem('token')
+      await axios.post('http://localhost:8080/categories', { nome }, {
         headers: {
-          Authorization: `Bearer ${token}`
+          Authorization: `Bearer ${ token }`
         }
       })
+      setNotification({
+        open: true,
+        message: `Categoria ${ nome } criada com sucesso!`,
+        severity: 'success'
+      })
+      handleCloseModal()
     }
-    catch(error){
-      console.log("error", error);
+    catch (error) {
+      setNotification({
+        open: true,
+        message: error.response.error,
+        severity: 'error'
+      })
     }
-    
   }
 
-  return(
-    <S.FormCategory>
-      <form onSubmit={onSubmit}>
-        <h1>Criar Categoria</h1>
-        <S.TextField type='name' id="name" label="name" name='name' variant="outlined" onChange={onChangeValue} />
-        <S.Button variant="contained" type="submit">Entrar</S.Button>
-      </form>
-    </S.FormCategory>
+  const handleClose = (_, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setNotification({
+      open: false,
+      message: '',
+      severity: ''
+    })
+  }
+
+  return (
+    <>
+      <S.Snackbar open={ notification.open } autoHideDuration={ 3000 } onClose={ handleClose }>
+        <S.Alert onClose={ handleClose } severity={ notification.severity } variant="filled" sx={{ width: '100%' }}>
+          { notification.message }
+        </S.Alert>
+      </S.Snackbar>
+
+      <Dialog open={open} onClose={handleCloseModal}>
+        <DialogTitle>Nova Categoria</DialogTitle>
+        <DialogContent>
+          <S.Form onSubmit={ onSumbmit }>
+            <S.TextField name="nome" onChange={ onChangeValue } label="Nome" variant="outlined" color='primary' fullWidth />
+          </S.Form>
+        </DialogContent>
+        <DialogActions style={{ display: 'flex', justifyContent: 'center' }}>
+          <S.Button variant="contained" color="success" type="submit" onClick={ onSumbmit }>Salvar</S.Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
 
-export default CategoriesCreate;
+export default CategoriesCreate
