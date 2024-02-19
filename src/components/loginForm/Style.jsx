8682ef8289d889e3b2111d121151fd22f5745097
Gguife <gguife747@gmail.com>
2024-02-19 17:59:18 -0300

    feat: Created loginForm and registerForm styles

--- src/components/loginForm/Style.jsx
+++ src/components/loginForm/Style.jsx
@@ -1,18 +1,44 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled'
+import ButtonMUI from '@mui/material/Button';
+import TextFieldMUI from '@mui/material/TextField';
+import AlertMUI from '@mui/material/Alert';
+import SnackbarMUI from '@mui/material/Snackbar';
+import TypographyMUI from '@mui/material/Typography';
+import LinkNEXT from 'next/link'
+import FormControlMUI from '@mui/material/FormControl';
+import InputLabelMUI from '@mui/material/InputLabel';
+import OutlinedInputMUI from '@mui/material/OutlinedInput';
+import InputAdornmentMUI from '@mui/material/InputAdornment';
+import IconButtonMUI from '@mui/material/IconButton';
+import VisibilityMUI from '@mui/icons-material/Visibility';
+import VisibilityOffMUI from '@mui/icons-material/VisibilityOff';
 
-import ButtonUI from "@mui/material/Button";
-import TextFieldUI from "@mui/material/TextField";
+export const Button = styled(ButtonMUI)``;
 
-export const FormLogin = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  margin: 0 auto;
-`;
+export const TextField = styled(TextFieldMUI)``;
 
-export const TextField = styled(TextFieldUI)`
-  border-radius: 10px;
-  width: 100%;
-`;
-export const Button = styled(ButtonUI)``;
+export const H1 = styled.h1``;
+
+export const Form = styled.form``;
+
+export const Alert = styled(AlertMUI)``;
+
+export const Snackbar = styled(SnackbarMUI)``;
+
+export const Typography = styled(TypographyMUI)``;
+
+export const Link = styled(LinkNEXT)``;
+
+export const FormControl = styled(FormControlMUI)``;
+
+export const InputLabel = styled(InputLabelMUI)``;
+
+export const OutlinedInput = styled(OutlinedInputMUI)``;
+
+export const InputAdornment = styled(InputAdornmentMUI)``;
+
+export const IconButton = styled(IconButtonMUI)``;
+
+export const Visibility = styled(VisibilityMUI)``;
+
+export const VisibilityOff = styled(VisibilityOffMUI)``;
